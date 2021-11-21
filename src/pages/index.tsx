2d9e1f36ebb1b346f10950c593543bbcbf344faa
Samael Melo <samaelmelo.dev@gmail.com>
2021-11-21 09:02:56 -0400

    using static site generation in my aplication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps} from "next"
+import {GetStaticProps} from "next"
 import {stripe} from "../services/stripe"
 
 // elemento head importado no next para fazer um title dinâmico por paágina
@@ -44,7 +44,7 @@ export default function Home({product}: HomeProps) {
 }
 
 
-export const getServerSideProps:GetServerSideProps  = async () =>{
+export const getStaticProps:GetStaticProps  = async () =>{
   
   const price = await stripe.prices.retrieve('price_1Jy1ijESiexfXKl7ROuUi7jZ')
 
@@ -61,6 +61,7 @@ export const getServerSideProps:GetServerSideProps  = async () =>{
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
