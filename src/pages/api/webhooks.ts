cad7852599841e1c8ee4b54344d125e1c436dbcc
Samael Melo <samaelmelo.dev@gmail.com>
2021-12-12 16:53:54 -0400

    não consigo ouvir webhooks no console

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,9 +1,57 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe"
+import {stripe} from "../../services/stripe"
+
+
+
+async function buffer(readable: Readable){
+  const chuncks = [];
+
+  for await (const chunk of readable){
+    chuncks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chuncks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (req: NextApiRequest, res: NextApiResponse){
-  console.log('evento recebido');
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === "POST"){
 
-  res.status(200).json({ok : true})
-  
+    const buf = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event
+
+    try{
+      event = stripe.webhooks.constructEvent(buf,secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (err){
+      return res.status(400).send(`Webhook error ${err.message}`)
+    }
+
+    const {type} = event
+
+    if(relevantEvents.has(type)){
+      console.log('Evento recebido', event)
+    }
+
+    res.json({ received : true })
+    
+  } else {
+    res.setHeader('Allow', 'Post')
+    res.status(405).end('Method not allowed')
+  }
 }
